30fc78a0bc2f26d777813f3dd4ce0add3ec79d79
vincexodus <vincent4552@gmail.com>
2023-02-28 04:09:38 +0800

    nav-bar not transparent, router-link need fix

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,25 +1,99 @@
 <template>
-    <div>   
-        <div class="flex">
-            <img src="https://cdn.discordapp.com/attachments/966648657996828708/1050212721032953926/club_logo.png" alt="APU BCC Logo" width="100">
-            <p class="font-Encode font-bold text-2xl my-5"> APU BCC </p>
-            <ul class="flex my-5">
-                <li> <router-link to="/about">About</router-link> </li> 
-                <li> <router-link to="/events">Events</router-link> </li>
-                <li> <router-link to="/resources">Resources</router-link> </li> 
-                <li> <router-link to="/faq">FAQ</router-link> </li> 
-            </ul>
-        </div>
+  <div class="flex bg-transparent p-3 justify-between w-4/5 m-auto">
+    <!-- <div class=""> -->
+    <div class="flex">
+      <img class="w-14 h-14" src="src\assets\Logo.png" alt="Logo" width="200">
+      <p class="font-Encode font-bold text-3xl text-white m-2"> APUBCC </p>
+    </div>
+
+    <div class="m-3">
+      <ul class="flex font-Encode font-extrabold text-lg text-white">
+
+        <button class="px-5 flex" type="button" @click="toggleAbtDropDown" :aria-expanded="isAbtDropDown.toString()">
+          ABOUT
+          <span class="my-auto ml-2 h-5 w-5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor"
+              stroke-width="2">
+              <path fill-rule="evenodd"
+                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                clip-rule="evenodd" />
+            </svg>
+          </span>
+        </button>
         <div>
-          
-        </div>   
-    </div> 
+          <ul v-if="isAbtDropDown" data-te-dropdown-ref
+            class="absolute z-[1000] top-10 right-7 min-w-max list-none overflow-hidden rounded-lg bg-transparent [&[data-te-dropdown-show]]:block bg-orange-400">
+            <li>
+              <a class="block w-full whitespace-nowrap py-2 px-4 text-lg  font-extrabold text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline 
+                disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600" 
+                href="#" data-te-dropdown-item-ref>Action</a>
+            </li>
+          </ul>
+        </div>
+
+
+        <!-- End About Nav -->
+        <li class="px-5">
+          <router-link to="/events">
+            <a href="" class="font-extrabold">EVENTS</a>
+          </router-link>
+        </li>
+        <!-- End Event Nav -->
+        <button class="px-5 flex" type="button" @click="toggleResDropDown" :aria-expanded="isResDropDown.toString()">
+          RESOURCES
+          <span class="my-auto ml-2 h-5 w-5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor"
+              stroke-width="2">
+              <path fill-rule="evenodd"
+                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                clip-rule="evenodd" />
+            </svg>
+          </span>
+        </button>
+        <div>
+          <ul v-if="isResDropDown" data-te-dropdown-ref
+            class="absolute z-[1000] top-10 right-7 min-w-max list-none overflow-hidden rounded-lg bg-transparent [&[data-te-dropdown-show]]:block bg-orange-400">
+            <li>
+              <a class="block w-full whitespace-nowrap py-2 px-4 text-lg  font-extrabold text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline 
+                disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600" 
+                href="#" data-te-dropdown-item-ref>RESOURCE 1</a>
+            </li>
+          </ul>
+        </div>
+        <!-- End Resource Nav -->
+
+        <li class="px-5">
+          <router-link to="/faq">
+            <a href="" class="font-extrabold">FAQ</a>
+          </router-link>
+        </li>
+        <button class="px-5 ml-5 rounded-full text-sm font-bold bg-orange-400 ">JAIL NOW</button>
+      </ul>
+
+    </div>
+
+    <!-- </div> -->
+
+  </div>
 </template>
 
 <script>
 
 export default {
-    name: "Navbar",
+  name: "Navbar",
+  data() {
+    return {
+      isAbtDropDown: false,
+      isResDropDown: false
+    };
+  },
+  methods: {
+    toggleAbtDropDown() {
+      this.isAbtDropDown = !this.isAbtDropDown;
+    },
+    toggleResDropDown() {
+      this.isResDropDown = !this.isResDropDown;
+    }
+  }
 }
-
 </script>
